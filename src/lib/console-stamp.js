bf840ff30c2d1161981bd9d177cbc3af47ba6d42
patrikx3 <alabard@gmail.com>
2020-05-17 15:28:49 +0200

    robo8x [chore]   5/17/2020, 3:28:49 PM

--- src/lib/console-stamp.js
+++ src/lib/console-stamp.js
@@ -1,31 +1,40 @@
 const chalk = require('chalk');
 const consoleStamp = () => {
 // overriding the console should be after this!!!
-    require('console-stamp')(console, {
-        format: ':date(yyyy/mm/dd HH:MM:ss.l).cyan :p3x.yellow :myLabel',
-        tokens:{
-            p3x: () => {
-                return chalk`{black.grey [P3XRS]}` + ` [PID: ${(String(process.pid).padStart(6, 0))}] `;
-            },
-            myLabel: ( arg ) => {
-                const { method, defaultTokens } = arg;
-                let label = defaultTokens.label( arg );
+
+    const methods = ['log', 'info', 'warn', 'error', 'debug']
+    const originalMethods = {}
+    for(let method of methods) {
+        originalMethods[method] = console[method]
+
+        console[method] = function() {
+            if (arguments[0]) {
+                let label
                 switch(method) {
                     case 'error':
-                        label = chalk`{bold.red ${label}}`;
+                        label = chalk`{bold.red ${method.toUpperCase()}}`;
                         break;
 
                     case 'warn':
-                        label = chalk`{bold.blue ${label}}`;
+                        label = chalk`{bold.blue ${method.toUpperCase()}}`;
                         break;
 
                     default:
-                        label = chalk`{green ${label}}`;
+                        label = chalk`{green ${method.toUpperCase()}}`;
                 }
-                return label;
+
+                let data = '' //chalk`${moment().format(`YYYY/MM/DD HH:mm:ss.SSS`)} `
+                data += chalk`{black.grey [P3XRS]}` + ` [PID: ${(String(process.pid).padStart(6, 0))}] `
+
+                //arguments[0] = data + arguments[0]
+                const mainArguments = Array.prototype.slice.call(arguments);
+                mainArguments.unshift(data);
+                originalMethods[method].apply(null, mainArguments)
+            } else {
+                originalMethods[method].apply(null, arguments)
             }
-        },
-    });
+        }
+    }
 }
 
 module.exports = consoleStamp
